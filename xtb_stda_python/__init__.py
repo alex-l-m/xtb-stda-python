b3ff0a904be4d4d36caf7d27657d97b2e696edcd
Alexander Davis <alexlovesmolecules@gmail.com>
2022-10-19 20:01:10 -0400

    Changed regex for extracting lowest excitation energy from stda log

--- xtb_stda_python/__init__.py
+++ xtb_stda_python/__init__.py
@@ -139,7 +139,12 @@ def log2energy(stda_log):
     to the lowest excited state as a number, in eV'''
     right_part = False
     for line in stda_log.split("\n"):
-        match_string = r"\s*1\s*([.0-9]*)"
+        # Based on observation (not inspection of the code) it looks like it
+        # always prints three digits after the decimal point. And with
+        # sufficiently small energies, there's not necessarily a space between
+        # the energy and the wavelength. Therefore the regex can't use * at the
+        # end, it has to actually count digits
+        match_string = r"\s*1\s*([0-9]*\.[0-9][0-9]?[0-9]?)"
         # Section heading
         if "excitation energies, transition moments and TDA amplitudes" in line:
             right_part = True
